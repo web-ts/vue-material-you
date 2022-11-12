2ad0d82dfbac2a6c7f8b2a33664f276aeda170ed
web-ts <hstudio.dev@gmail.com>
2022-11-12 19:51:32 +0200

    :white_check_mark: Updated test coverage.

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,12 @@ export default defineConfig({
       inline: [
         "@material/material-color-utilities"
       ]
+    },
+    coverage: {
+      lines: 80,
+      functions: 80,
+      branches: 80,
+      statements: 80,
     }
   },
   resolve: {
